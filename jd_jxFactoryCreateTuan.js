53c51bf883570cba1c71f4aa2a13ab54609261d3
lxk0301 <778732665@qq.com>
2021-01-28 18:38:55 +0800

    jd_updateFactoryTuanId.json

--- jd_jxFactoryCreateTuan.js
+++ jd_jxFactoryCreateTuan.js
@@ -7,7 +7,7 @@ const JD_API_HOST = 'https://m.jingxi.com';
 const fs = require('fs');
 const notify = $.isNode() ? require('./sendNotify') : '';
 let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
-const tuanActiveId = `4WbghxAbaDGMjxX48Mt5XA%3D%3D`;
+const tuanActiveId = `4WbghxAbaDGMjxX48Mt5XA==`;
 let cookiesArr = [], cookie = '', message = '';
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 Object.keys(jdCookieNode).forEach((item) => {
