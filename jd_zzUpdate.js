e4757ae7dff94a48ae719c77e8dcf9e2bd2a0edb
lxk0301 <778732665@qq.com>
2021-04-12 17:58:50 +0800

    fix

--- jd_zzUpdate.js
+++ jd_zzUpdate.js
@@ -64,7 +64,7 @@ async function jdWish() {
   $.hasOpen = false;
   $.assistStatus = 0;
   await getUserTuanInfo()
-  if (!$.tuan && $.assistStatus !== 3) {
+  if (!$.tuan && $.assistStatus === 3 && $.canStartNewAssist) {
     console.log(`准备再次开团`)
     await openTuan()
     if ($.hasOpen) await getUserTuanInfo()
