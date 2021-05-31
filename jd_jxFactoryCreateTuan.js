063f3fde2f957c48ac7a0b7ce820378a5f44a2ef
lxk0301 <12345677@qq.com>
2021-05-31 15:07:11 +0800

    jd_jxFactoryCreateTuan.js

--- jd_jxFactoryCreateTuan.js
+++ jd_jxFactoryCreateTuan.js
@@ -2,7 +2,7 @@
 京东京喜工厂自动开团
  */
 const $ = new Env('京东京喜工厂自动开团');
-const tuanActiveId = `u9pm44YKSBNDQTgn7sOCEg==`;
+const tuanActiveId = `P6Nwt8wkOa-cyMNFcmhNZw==`;
 const JD_API_HOST = 'https://m.jingxi.com';
 const fs = require('fs');
 const notify = $.isNode() ? require('./sendNotify') : '';
